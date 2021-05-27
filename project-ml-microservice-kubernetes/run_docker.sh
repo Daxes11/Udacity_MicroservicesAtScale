f92ae1036a12adf177d7d509a00c02729b55bd01
Daxes11 <xcucumax@gmail.com>
2021-05-27 20:37:19 +0000

    initial commit

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -tag udacity .
+docker build -t udacity .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -it udacity bash
+docker run -p 8000:80 udacity 
